62312309ed39c741b53178a911638036c037ed6d
panospd <anastapd@gmail.com>
2019-03-20 17:36:12 +0000

    Implemented Advert api

---
+++ AdvertApi/Controllers/Advert.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Amazon.DynamoDBv2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/adverts")]
+    public class Advert : Controller
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse {Id = recordId});
+        }
+
+        [HttpPut]
+        [Route("confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+            return new OkResult();
+        }
+    }
+}
