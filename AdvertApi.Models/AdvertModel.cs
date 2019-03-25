3ab5d7f5a64f7143192c9c14e44864ef999479ad
panospd <anastapd@gmail.com>
2019-03-25 14:55:38 +0000

    Added username to AdvertModel and updated the nuget package

--- AdvertApi.Models/AdvertModel.cs
+++ AdvertApi.Models/AdvertModel.cs
@@ -7,5 +7,6 @@ namespace AdvertApi.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+        public string Username { get; set; }
     }
 }
